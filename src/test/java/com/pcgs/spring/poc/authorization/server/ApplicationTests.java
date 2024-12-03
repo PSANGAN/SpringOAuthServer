f358663076147ee3d5f1fdf4c7673a0a56ccc2c5
PSANGAN <32886735+PSANGAN@users.noreply.github.com>
2024-12-02 22:46:13 -0500

    Init

---
+++ src/test/java/com/pcgs/spring/poc/authorization/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcgs.spring.poc.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
