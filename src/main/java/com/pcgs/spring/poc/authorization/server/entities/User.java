ded337c594bd8a3c392dd41b0aea66c5f1480261
PSANGAN <32886735+PSANGAN@users.noreply.github.com>
2024-12-07 19:25:43 -0500

    AuthoritiesChanges

--- src/main/java/com/pcgs/spring/poc/authorization/server/entities/User.java
+++ src/main/java/com/pcgs/spring/poc/authorization/server/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -18,10 +20,10 @@ public class User {
 
     private String password;
 
-//    @ManyToMany(fetch = FetchType.EAGER)
-//    @JoinTable(name = "users_authorities",
-//            joinColumns = @JoinColumn(name = "user_id"),
-//            inverseJoinColumns = @JoinColumn(name = "authority_id")
-//    )
-//    private Set<Authority> authorities;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_authorities",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
+    private Set<Authority> authorities;
 }
---
+++ src/main/java/com/pcgs/spring/poc/authorization/server/security/SecurityAuthority.java
@@ -0,0 +1,16 @@
+package com.pcgs.spring.poc.authorization.server.security;
+
+import com.pcgs.spring.poc.authorization.server.entities.Authority;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@AllArgsConstructor
+public class SecurityAuthority implements GrantedAuthority {
+
+    private final Authority authority;
+
+    @Override
+    public String getAuthority() {
+        return authority.getName();
+    }
+}
